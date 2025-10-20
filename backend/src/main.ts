2619202b2b88fa40fb3011a23e52b80329509d32
Valentino Privitera <devalen543@gmail.com>
2025-10-20 19:12:04 -0300

    CORS para Prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://mykanban-ten.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
